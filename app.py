76e745dd451299ed41a66e36282a615d9dbbe830
L1nkr3rrr <l1nk3rino@gmail.com>
2023-09-12 18:25:40 +0300

    [HILL-002] DEV - endpoint for returning an average data from csv file, also added a query parameter 'rounding' that can manipulate a rounding of digits (by default it's 2)

--- app.py
+++ app.py
@@ -28,5 +28,23 @@ def generate_users():
     return make_response('</br>'.join(utils.get_fake_users(qty_of_users)))
 
 
+@app.route("/mean/")
+def get_measurements_csv():
+    rounding_factor_str = request.args.get('rounding', '2')
+    try:
+        rounding_factor = int(rounding_factor_str)
+        if rounding_factor < 0:
+            raise ValueError("Rounding factor must be a non-negative integer.")
+    except ValueError as e:
+        logger.error(e)
+        return make_response("Invalid rounding factor. Please provide a non-negative integer.")
+
+    avg_height, avg_weight = utils.get_avg_data_from_csv('hw.csv')
+    return make_response(
+        f"Average Height - {avg_height:.{rounding_factor}f}</br>"
+        f"Average Weight - {avg_weight:.{rounding_factor}f}"
+    )
+
+
 if __name__ == "__main__":
     app.run()
